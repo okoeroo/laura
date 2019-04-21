23a4c675a90fbc9460b9feaf465d42db7ee0a766
Oscar Koeroo <okoeroo@gmail.com>
2019-04-21 11:42:07 +0200

    Added vital information

---
+++ modules/refresh_database.sh
@@ -0,0 +1,3 @@
+curl https://iptoasn.com/data/ip2country-v4.tsv.gz | gunzip -d > ip2country-v4.tsv
+curl https://iptoasn.com/data/ip2country-v6.tsv.gz | gunzip -d > ip2country-v6.tsv
+curl https://iptoasn.com/data/ip2asn-combined.tsv.gz | gunzip -d > ip2asn-combined.tsv
