3ff3c5712fd3a02ed4a610a01448d08bb069ca4f
Oscar Koeroo <okoeroo@gmail.com>
2019-06-10 21:39:15 +0200

    New feat to step2

--- step2.sh
+++ step2.sh
@@ -1,1 +1,8 @@
-./laura-create-work.py --fb-apikey $(cat facebook-app.apikey)  --error-file error.list --couch-user okoeroo --couch-pw qwerty123 --batch-size 200
+#!/bin/bash
+
+BATCH=$1
+if [ -z "$BATCH" ]; then
+    BATCH=200
+fi
+
+./laura-create-work.py --fb-apikey $(cat facebook-app.apikey)  --error-file error.list --couch-user okoeroo --couch-pw qwerty123 --batch-size $BATCH
